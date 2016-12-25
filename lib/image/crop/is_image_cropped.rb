2d452f20eb74339ab165920ff7e8c4dda9b5caaa
banta <banta.bernard@gmail.com>
2016-12-25 13:16:41 +0300

    Save image with extension

--- lib/image/crop/is_image_cropped.rb
+++ lib/image/crop/is_image_cropped.rb
@@ -6,7 +6,7 @@ class ActiveRecord::Base
       attr_names.each do |attr|
         if eval("self.#{attr}")
           image = Paperclip.io_adapters.for(eval("self.#{attr}"))
-          image.original_filename = "cropped_file_#{Time.now.to_f.to_s.gsub('.','')}"
+          image.original_filename = "cropped_file_#{Time.now.to_f.to_s.gsub('.','')}.#{self.picture_content_type.split('/').last}"
           eval("self.#{attr} = image")
         end
       end
